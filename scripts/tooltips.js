86ca2fcc2e79fd242b2700da9656de1e821c4b6f
Partario Flynn <partario.flynn@gmail.com>
2025-06-15 17:20:56 -0400

    Delete scripts/tooltips.js

--- scripts/tooltips.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-    $("a[href^=\\#define-]")
-        .tooltip({
-            html: true,
-            title: function () {
-                return $(this.href.substring(this.href.lastIndexOf("#"))).clone().wrap('<div></div>').parent()
-            }
-        })
-})
